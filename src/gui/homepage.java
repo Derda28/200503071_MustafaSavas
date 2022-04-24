097220f1ef534ab4b36ffb85ff36a6a4fb45432e
Derda28 <derdasavas67@gmail.com>
2022-04-24 16:02:02 +0300

    Homepage fertig, dbHelper fertig.

---
+++ src/gui/homepage.java
@@ -0,0 +1,37 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class homepage extends JFrame{
+    private JButton customerBtn;
+    private JButton menuBtn;
+    private JButton abonnementBtn;
+    private JLabel titleLbl;
+
+    public homepage() {
+
+        customerBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        menuBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        abonnementBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
+}
